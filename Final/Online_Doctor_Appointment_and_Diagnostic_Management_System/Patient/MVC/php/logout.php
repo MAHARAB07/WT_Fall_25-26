e3e576f457f8a81f35d364ced3bc7e585ba15079
MAHARAB07 <maharabsaifi365@gmail.com>
2026-01-17 22:18:48 +0600

    logout logig handled

--- Final/Online_Doctor_Appointment_and_Diagnostic_Management_System/Patient/MVC/php/logout.php
+++ Final/Online_Doctor_Appointment_and_Diagnostic_Management_System/Patient/MVC/php/logout.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-
-/* সব session data remove */
 session_unset();
-
-/* session destroy */
 session_destroy();
-
-/* login page এ redirect */
 header("Location: ../html/login.php");
 exit();
